c274ec31ef53a5ac8bf4dfef51a03ee736c444a1
aa6977-cell <aa6977@srmist.edu.in>
2026-03-03 08:13:53 +0530

    uc1:oopsbanner

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,3 +1,13 @@
+/**
+ * OOPSBanner UC1 -OOPS banner display application 
+ * 
+ * this class demonstration a simple java application that display the object 
+ * oriented programming system OOPS acronym to the console 
+ * 
+ * @author developer 
+ * @version 1.0
+ */
+
 public class  OOPSBanner {
       public static void main(String[]args){
             System.out.print("OOPS");    
